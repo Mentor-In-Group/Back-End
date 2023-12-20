73291092325be45b66607ed167051e9148ff1eb9
ym010900 <76116676+ym010900@users.noreply.github.com>
2023-12-20 21:04:47 +0700

    Delete src/routes/bookingRoute.js

--- src/routes/bookingRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { bookSchedule, getBookingById } from "../controllers/bookingController.js";
-const bookingRouter = express.Router();
-
-bookingRouter.post("/booking", bookSchedule);
-bookingRouter.get("/booking", getBookingById);
-
-export default bookingRouter;
