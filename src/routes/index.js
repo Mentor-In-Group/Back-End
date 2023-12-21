5b93df85f52c3172fd89349ee5cd93ca2ada95e1
ym010900 <76116676+ym010900@users.noreply.github.com>
2023-12-21 13:52:38 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import userRouter from "./userRouter.js";
 import { errorrHandling } from "../controllers/errorHandling.js";
 import scheduleRouter from "./scheduleRouter.js";
+import mentorRouter from "./mentorRoute.js";
 const route = express.Router();
 
 route.use("/mentorin", userRouter);
 route.use("/mentorin", scheduleRouter);
+route.use("/mentorin", mentorRouter);
 route.use("*", errorrHandling);
 route.use("*", (req, res) => {
     res.status(404).json({
